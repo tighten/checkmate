fbb0782754349c18a7a3e4a6a023e978c2f86425
Matt Stauffer <matt@tighten.co>
2018-08-10 23:20:59 -0400

    update readme

--- readme.md
+++ readme.md
@@ -5,3 +5,7 @@
 
 1. Make a new token with `repo` scope: https://github.com/settings/tokens/new
 2. Save it in `.env` as `GITHUB_TOKEN`
+
+## Next steps
+- get latest of 5.4.*, 5.5.*, etc. from laravel to compare against
+- stuff
--- routes/web.php
+++ routes/web.php
@@ -3,10 +3,6 @@
 use App\Project;
 
 Route::get('/', function () {
-    // dd(laravelActualFromComposerLock(composerLockForRepo('tightenco', 'symposium')));
-    // dd(composerLockForRepo('tightenco', 'symposium'));
-    // dd(laravelConstraintFromComposerJson(composerJsonForRepo('tightenco', 'symposium')));
-
     return view('welcome', [
         'projects' => Project::all(),
     ]);
