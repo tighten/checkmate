e1d5531ae6d7d48d7218f9e959e33e82837fa69a
Chris Trombley <ctroms@gmail.com>
2019-02-08 17:32:57 -0500

    Add tighten packages to proejct seeder

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -5,13 +5,10 @@ use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     *
-     * @return void
-     */
     public function run()
     {
-        factory(Project::class, 5)->create();
+    	$this->call([
+    		ProjectsSeeder::class
+    	]);
     }
 }
