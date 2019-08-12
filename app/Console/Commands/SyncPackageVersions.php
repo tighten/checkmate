cfd76c6b68caea630960a4a9ca2d051afaf52f48
Chris Trombley <ctroms@gmail.com>
2019-08-11 20:50:37 -0400

    Add command to warm the cache with package version info

---
+++ app/Console/Commands/SyncPackageVersions.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Project;
+use Github\Client as GitHubClient;
+use Exception;
+use Illuminate\Console\Command;
+
+class SyncPackageVersions extends Command
+{
+    protected $signature = 'sync:packageversions {package?}';
+
+    protected $description = 'Warm the cache with version info of all active projects that aren\'t already cached. Specify \'vendor/package\' to hydrade a single package';
+
+    protected $client;
+
+    public function __construct(GitHubClient $client)
+    {
+        parent::__construct();
+
+        $this->client = $client;
+    }
+
+    public function handle()
+    {
+        $projects = $this->getProjects();
+
+        $bar = $this->output->createProgressBar($projects->count());
+        $bar->start();
+
+        $projects->map(function ($project, $key) use ($bar) {
+            // For now we don't care what the exception was that was thrown by the GitInfoParser or
+            // the LaravelVersions parser. We swallow the exception and continue.
+            try {
+                $this->syncProjectVersion($project);
+            } catch (Exception $e) {
+            }
+
+            $bar->advance();
+        });
+
+        $bar->finish();
+    }
+
+    protected function syncProjectVersion($project)
+    {
+        $project->laravel_constraint;
+        $project->laravel_version;
+        $project->desired_laravel_version;
+    }
+
+    protected function getProjects()
+    {
+        if (is_null($this->argument('package'))) {
+            return Project::active()->get();
+        }
+
+        list($vendor, $package) = explode("/", $this->argument('package'));
+        $project = [Project::active()->where('package', $package)->first()] ?? [];
+
+        return collect($project);
+    }
+}
