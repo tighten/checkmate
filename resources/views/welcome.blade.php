ec21bf879a7187c486cd8358d342c6f16c577ec9
Marcus Moore <contact@marcusmoore.io>
2020-07-13 11:16:45 -0700

    Extract header partial

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,24 +12,8 @@
     </head>
 
     <body>
-        <div class="bg-white border-t-4 border-indigo relative z-10 shadow">
-            <div class="p-2">
-                <section class="max-w-xl mx-auto">
-                    <div class="flex justify-between items-center">
-                        <p class="flex items-center">
-                            <a
-                                href="{{ route('project.index') }}"
-                                class="uppercase text-2xl leading-normal text-black-light font-semibold font-open-sans tracking-wide no-underline"
-                            >
-                                Checkmate
-                            </a>
-                        </p>
 
-                        <p class="italic font-thin leading-normal text-grey-blue-darkest">Catchy phrase I guess?</p>
-                    </div>
-                </section>
-            </div>
-        </div>
+        @include('partials.header')
 
         <div class="bg-frost font-sans relative z-0">
             <div class="max-w-xl mx-auto pt-8">
