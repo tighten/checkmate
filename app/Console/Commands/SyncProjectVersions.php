6d322f99a2bc075cddaeeb61a884ff172a6a8bb8
Marcus Moore <contact@marcusmoore.io>
2020-07-16 11:41:25 -0700

    WIP: Start to update projects via GraphQL

--- app/Console/Commands/SyncProjectVersions.php
+++ app/Console/Commands/SyncProjectVersions.php
@@ -3,48 +3,171 @@
 namespace App\Console\Commands;
 
 use App\Project;
-use Github\Client as GitHubClient;
 use Illuminate\Console\Command;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Http;
 
 class SyncProjectVersions extends Command
 {
-    protected $signature = 'sync:project-versions {project?}';
+    protected $signature = 'sync:project-versions';
 
-    protected $description = 'Warm the cache with version info of all active projects that aren\'t already cached. Specify \'vendor/repo\' to hydrate a single project';
+    protected $description = 'Update all project versions from GitHub.';
 
-    protected $client;
+    protected $repositories;
 
-    public function __construct(GitHubClient $client)
-    {
-        parent::__construct();
-
-        $this->client = $client;
-    }
+    private $defaultFilters = [
+        'first' => '100',
+        'isFork' => 'false',
+        'orderBy' => '{field: CREATED_AT, direction: ASC}',
+    ];
 
     public function handle()
     {
-        $projects = $this->getProjects();
+        do {
+            $filters = $this->formatRepositoryFilters();
 
-        $bar = $this->output->createProgressBar($projects->count());
-        $bar->start();
+            $query = <<<QUERY
+            {
+              organization(login: "tightenco") {
+                id
+                repositories($filters) {
+                  totalCount
+                  edges {
+                    node {
+                      name
+                      owner {
+                        login
+                      }
+                      composerJson: object(expression: "master:composer.json") {
+                        id
+                        ... on Blob {
+                          text
+                        }
+                      }
+                      composerLock: object(expression: "master:composer.lock") {
+                        id
+                        ... on Blob {
+                          text
+                        }
+                      }
+                    }
+                  }
+                  pageInfo {
+                    endCursor
+                    hasNextPage
+                  }
+                }
+              }
+              rateLimit {
+                cost
+                limit
+                remaining
+                resetAt
+              }
+            }
+            QUERY;
 
-        $projects->map(function (Project $project) use ($bar) {
-            $project->syncLaravelVersionAndConstraint();
-            $bar->advance();
+            $response = Http::withToken(config('services.github.token'))
+                ->post('https://api.github.com/graphql', ['query' => $query])
+                ->json();
+
+            if (array_key_exists('errors', $response)) {
+                // @todo: proper error handling
+                dd('problem...', $response);
+            }
+
+            $this->addRepositoriesFromResponse($response);
+
+            $nextPage = $this->getNextPage($response);
+
+            if ($nextPage) {
+                $this->setNextPage($response);
+            }
+
+        } while ($nextPage);
+
+        $this->repositories->flatten(1)->each(function ($repository) {
+            $project = Project::firstOrCreate([
+                'name' => $repository['name'],
+                'vendor' => $repository['vendor'],
+                'package' => $repository['name'],
+            ], [
+                'current_laravel_version' => $repository['current_version'],
+                'current_laravel_constraint' => $repository['constraint'],
+                'is_valid' => true,
+            ]);
+
+            if ($project->current_laravel_version !== $repository['current_version'] || $project->current_laravel_constraint !== $repository['constraint']) {
+                $project->update([
+                    'current_laravel_version' => $repository['current_version'],
+                    'current_laravel_constraint' => $repository['constraint'],
+                    'is_valid' => true,
+                ]);
+            }
         });
 
-        $bar->finish();
+        $this->info('Done...');
     }
 
-    protected function getProjects()
+    private function addRepositoriesFromResponse(array $response)
     {
-        if (is_null($this->argument('project'))) {
-            return Project::active()->get();
+        if (! $this->repositories instanceof Collection) {
+            $this->repositories = collect();
         }
 
-        list($vendor, $project) = explode("/", $this->argument('project'));
-        $project = [Project::active()->where('project', $project)->first()] ?? [];
+        $this->repositories->push(
+            collect(data_get($response, 'data.organization.repositories.edges'))
+                ->filter(function ($repository) {
+                    return $repository['node']['composerLock'];
+                })
+                ->map(function ($repository) {
 
-        return collect($project);
+                    $vendor = data_get($repository, 'node.owner.login');
+
+                    $composerJson = json_decode(data_get($repository, 'node.composerJson.text'), true);
+
+                    $constraint = data_get($composerJson, 'require.laravel/framework');
+
+                    $laravelFramework = collect(
+                        json_decode(
+                            data_get($repository, 'node.composerLock.text'),
+                            true
+                        )['packages'])
+                        ->firstWhere('name', 'laravel/framework');
+
+                    $laravelVersion = ltrim($laravelFramework['version'] ?? null, 'v');
+
+                    return [
+                        'vendor' => $vendor,
+                        'name' => $repository['node']['name'],
+                        'current_version' => $laravelVersion,
+                        'constraint' => $constraint,
+                    ];
+                })
+                ->filter(function ($repository) {
+                    return $repository['current_version'];
+                })
+                ->values()
+        )->flatten(1);
+    }
+
+    private function formatRepositoryFilters()
+    {
+        return collect($this->defaultFilters)
+            ->map(function ($value, $key) {
+                return "{$key}: $value";
+            })
+            ->implode(', ');
+    }
+
+    private function getNextPage(array $response)
+    {
+        return data_get($response, 'data.organization.repositories.pageInfo')['endCursor'];
+    }
+
+    private function setNextPage(array $response)
+    {
+        $this->defaultFilters['after'] = '"' . data_get($response,
+                'data.organization.repositories.pageInfo')['endCursor'] . '"';
     }
 }
--- app/Project.php
+++ app/Project.php
@@ -16,21 +16,11 @@ class Project extends Model
         'is_valid' => 'boolean',
     ];
 
-    public function getLaravelConstraintAttribute()
-    {
-        return app(GitInfoParser::class)->laravelConstraint($this->vendor, $this->package);
-    }
-
-    public function getLaravelVersionAttribute()
-    {
-        // @todo: go away from this method...have a job sync these periodically and just pull from db?
-        return app(GitInfoParser::class)->laravelVersion($this->vendor, $this->package);
-    }
-
     public function getDesiredLaravelVersionAttribute()
     {
         [$major, $minor] = explode('.', $this->current_laravel_version);
 
+        // @todo: cache this
         $version = LaravelVersion::where([
             'major' => $major,
             'minor' => $minor,
@@ -46,7 +36,7 @@ class Project extends Model
 
     public function getIsBehindLatestAttribute()
     {
-        return version_compare($this->desired_laravel_version, $this->laravel_version) > 0;
+        return version_compare($this->desired_laravel_version, $this->current_laravel_version) > 0;
     }
 
     public function scopeActive($query)
@@ -62,37 +52,5 @@ class Project extends Model
     public function scopeValid($query)
     {
         return $query->where('is_valid', 1);
-    }
-
-    public function syncLaravelVersionAndConstraint()
-    {
-        try {
-            $currentVersion = app(GitInfoParser::class)->laravelVersion($this->vendor, $this->package);
-            $constraint = app(GitInfoParser::class)->laravelConstraint($this->vendor, $this->package);
-
-            $updates = [
-                'last_synced_at' => now(),
-            ];
-
-            if ($this->current_laravel_version !== $currentVersion) {
-                $updates['current_laravel_version'] = $currentVersion;
-            }
-
-            if ($this->current_laravel_constraint !== $constraint) {
-                $updates['current_laravel_constraint'] = $constraint;
-            }
-
-            if (! $this->is_valid) {
-                $updates['is_valid'] = true;
-            }
-
-            $this->update($updates);
-
-        } catch (ComposerJsonFileNotFound | ComposerLockFileNotFound | NotALaravelProject $e) {
-            $this->update([
-                'is_valid' => false,
-                'last_synced_at' => now(),
-            ]);
-        }
     }
 }
