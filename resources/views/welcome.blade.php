ac8432e8ed84dcfa9b2d3689a7e39695959d5608
Marcus Moore <contact@marcusmoore.io>
2020-09-25 14:29:10 -0700

    Move projects that are behind to the top of the list

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,36 +22,6 @@
             </ul>
 
             <section class="bg-white rounded-b-lg">
-                @foreach ($current as $project)
-                    <ul class="flex list-reset p-4 border-t border-smoke">
-                        <li class="w-2/6">
-                            <a class="text-indigo-700 hover:text-indigo-900 no-underline text-md" href="{{ $project->github_url }}">
-                                {{ $project->name }}
-                            </a>
-                        </li>
-
-                        <li class="w-1/6 text-black-lightest">{{ $project->current_laravel_constraint }}</li>
-
-                        <li class="w-1/6 text-black-lightest">{{ $project->current_laravel_version }}</li>
-
-                        <li class="w-1/6 text-black-lightest">{{ $project->desired_laravel_version }}</li>
-
-                        <li class="w-1/6 text-black-lightest">
-                            <span class="text-green-700">CURRENT</span>
-                        </li>
-
-                        <li class="w-1/6">
-                            <form action="{{ route('project.ignore', $project) }}" method="POST">
-                                @method('PATCH')
-                                @csrf
-                                <input type="submit" value="Ignore" class="bg-indigo-600 hover:bg-indigo-500 text-white px-3 py-1 rounded cursor-pointer">
-                            </form>
-                        </li>
-                    </ul>
-                @endforeach
-            </section>
-
-            <section class="bg-white rounded-b-lg">
                 @foreach ($behind as $project)
                     <ul class="flex list-reset p-4 border-t border-smoke">
                         <li class="w-2/6">
@@ -81,6 +51,35 @@
                 @endforeach
             </section>
 
+            <section class="bg-white rounded-b-lg">
+                @foreach ($current as $project)
+                    <ul class="flex list-reset p-4 border-t border-smoke">
+                        <li class="w-2/6">
+                            <a class="text-indigo-700 hover:text-indigo-900 no-underline text-md" href="{{ $project->github_url }}">
+                                {{ $project->name }}
+                            </a>
+                        </li>
+
+                        <li class="w-1/6 text-black-lightest">{{ $project->current_laravel_constraint }}</li>
+
+                        <li class="w-1/6 text-black-lightest">{{ $project->current_laravel_version }}</li>
+
+                        <li class="w-1/6 text-black-lightest">{{ $project->desired_laravel_version }}</li>
+
+                        <li class="w-1/6 text-black-lightest">
+                            <span class="text-green-700">CURRENT</span>
+                        </li>
+
+                        <li class="w-1/6">
+                            <form action="{{ route('project.ignore', $project) }}" method="POST">
+                                @method('PATCH')
+                                @csrf
+                                <input type="submit" value="Ignore" class="bg-indigo-600 hover:bg-indigo-500 text-white px-3 py-1 rounded cursor-pointer">
+                            </form>
+                        </li>
+                    </ul>
+                @endforeach
+            </section>
         </div>
     </div>
     <br><br>
