46a945ff20bced90841ba107fedf38c5c56a4ef7
Ralph Lagumen <ralphlagumen-tc24@gmail.com>
2020-10-01 17:07:58 +0800

    Remove key on foreach

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -52,7 +52,7 @@
             </section>
 
             <section class="bg-white rounded-b-lg">
-                @foreach ($current as $key => $project)
+                @foreach ($current as project)
                     <ul class="flex list-reset p-4 border-t border-smoke bg-green-100">
                         <li class="w-2/6">
                             <a class="text-indigo-700 hover:text-indigo-900 no-underline text-md" href="{{ $project->github_url }}">
