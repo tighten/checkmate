8281ca90e289f4a425b03c5debf991d24db2356b
Matt Stauffer <matt@tighten.co>
2021-01-20 09:59:01 -0500

    Add orange background color for behind-but-not-insecure projects

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,38 +1,45 @@
 <?php
+use App\Project;
+
 $colorByStatus = [
-    App\Project::STATUS_CURRENT => 'text-green-400',
-    App\Project::STATUS_BEHIND => 'text-red-700',
-    App\Project::STATUS_INSECURE => 'text-red-700 font-bold',
-]
+    Project::STATUS_CURRENT => 'text-green-400',
+    Project::STATUS_BEHIND => 'text-orange-600',
+    Project::STATUS_INSECURE => 'text-red-700 font-bold',
+];
+$backgroundColorByStatus = [
+    Project::STATUS_CURRENT => 'bg-green-100',
+    Project::STATUS_BEHIND => 'bg-orange-100',
+    Project::STATUS_INSECURE => 'bg-red-100',
+];
 ?>
 @extends('layouts.app')
 
 @section('content')
-<div class="bg-gray-100 font-sans relative z-0">
-    <div class="max-w-6xl mx-auto pt-8">
+<div class="relative z-0 font-sans bg-gray-100">
+    <div class="max-w-6xl pt-8 mx-auto">
         <p class="mb-6 text-black-lighter">
             Showing versions for <span id="project_counter">{{ $projects->count() }}</span> active projects and packages
         </p>
         <div class="rounded-lg shadow">
-            <ul class="bg-gray-400 flex list-reset p-4 rounded-t-lg border-gray border-b-2">
-                <li class="w-2/6 font-semibold uppercase text-xs tracking-wide">Project / Package name</li>
+            <ul class="flex p-4 bg-gray-400 border-b-2 rounded-t-lg list-reset border-gray">
+                <li class="w-2/6 text-xs font-semibold tracking-wide uppercase">Project / Package name</li>
 
-                <li class="w-1/6 font-semibold uppercase text-xs tracking-wide">Version Constraint</li>
+                <li class="w-1/6 text-xs font-semibold tracking-wide uppercase">Version Constraint</li>
 
-                <li class="w-1/6 font-semibold uppercase text-xs tracking-wide">Current Version</li>
+                <li class="w-1/6 text-xs font-semibold tracking-wide uppercase">Current Version</li>
 
-                <li class="w-1/6 font-semibold uppercase text-xs tracking-wide">Prescribed Version</li>
+                <li class="w-1/6 text-xs font-semibold tracking-wide uppercase">Prescribed Version</li>
 
-                <li class="w-1/6 font-semibold uppercase text-xs tracking-wide">Status</li>
+                <li class="w-1/6 text-xs font-semibold tracking-wide uppercase">Status</li>
 
-                <li class="w-1/6 font-semibold uppercase text-xs tracking-wide">Ignore</li>
+                <li class="w-1/6 text-xs font-semibold tracking-wide uppercase">Ignore</li>
             </ul>
 
             <section class="bg-white rounded-b-lg">
                 @foreach ($projects as $project)
-                    <ul id="project_{{ $project->id }}" class="flex list-reset p-4 border-t border-smoke {{ $project->status === App\Project::STATUS_CURRENT ? 'bg-green-100' : 'bg-red-100' }}">
+                    <ul id="project_{{ $project->id }}" class="flex list-reset p-4 border-t border-smoke {{ $backgroundColorByStatus[$project->status] }}">
                         <li class="w-2/6">
-                            <a class="text-indigo-700 hover:text-indigo-900 no-underline text-md" href="{{ $project->github_url }}">
+                            <a class="text-indigo-700 no-underline hover:text-indigo-900 text-md" href="{{ $project->github_url }}">
                                 {{ $project->name }}
                             </a>
                         </li>
@@ -48,7 +55,7 @@ $colorByStatus = [
                         </li>
 
                         <li class="w-1/6">
-                            <button onClick="ignoreProject({{$project->id}});" type="button" class="bg-indigo-600 hover:bg-indigo-500 text-white px-3 py-1 rounded cursor-pointer">Ignore</button>
+                            <button onClick="ignoreProject({{$project->id}});" type="button" class="px-3 py-1 text-white bg-indigo-600 rounded cursor-pointer hover:bg-indigo-500">Ignore</button>
                         </li>
                     </ul>
                 @endforeach
