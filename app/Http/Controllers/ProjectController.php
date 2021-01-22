f7b284a8de93eb625360002ace9b3874366410b9
Matt Stauffer <matt@tighten.co>
2021-01-22 14:05:30 -0500

    Sort projects within each section by version number

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -9,14 +9,18 @@ class ProjectController extends Controller
     public function index()
     {
         $projects = Project::valid()->active()->get()->sortBy(function ($project) {
+            [$major, $minor, $patch] = explode('.', $project->current_laravel_version);
+
             // 1st Insecure | 2nd Behind | 3rd Current
             if ($project->status === Project::STATUS_INSECURE) {
-                return 1;
+                return 10000 + ($major * 1000) + ($minor * 100) + (int) $patch;
             }
+
             if ($project->status === Project::STATUS_BEHIND) {
-                return 2;
+                return 20000 + ($major * 1000) + ($minor * 100) + (int) $patch;
             }
-            return 3;
+
+            return 30000 + ($major * 100) + ($minor * 100) + (int) $patch;
         });
 
         if (auth()->guest() && ! (config('app.show_private_repos'))) {
