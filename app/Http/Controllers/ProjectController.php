df4ac62a418223dfe2772c8464ed915a6ce51234
Matt Stauffer <matt@tighten.co>
2021-01-22 14:07:10 -0500

    Fix sorting for double digit minors

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -13,14 +13,14 @@ class ProjectController extends Controller
 
             // 1st Insecure | 2nd Behind | 3rd Current
             if ($project->status === Project::STATUS_INSECURE) {
-                return 10000 + ($major * 1000) + ($minor * 100) + (int) $patch;
+                return 1000000 + ($major * 10000) + ($minor * 100) + (int) $patch;
             }
 
             if ($project->status === Project::STATUS_BEHIND) {
-                return 20000 + ($major * 1000) + ($minor * 100) + (int) $patch;
+                return 2000000 + ($major * 10000) + ($minor * 100) + (int) $patch;
             }
 
-            return 30000 + ($major * 100) + ($minor * 100) + (int) $patch;
+            return 3000000 + ($major * 100) + ($minor * 100) + (int) $patch;
         });
 
         if (auth()->guest() && ! (config('app.show_private_repos'))) {
