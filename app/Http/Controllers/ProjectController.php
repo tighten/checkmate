b527fe2a01c49739ad3e79dec9775695db4fbade
Matt Stauffer <matt@tighten.co>
2021-01-22 21:32:50 -0500

    Refactor sort with @bakerkretzmar's lovely work

--- app/Http/Controllers/ProjectController.php
+++ app/Http/Controllers/ProjectController.php
@@ -8,20 +8,12 @@ class ProjectController extends Controller
 {
     public function index()
     {
-        $projects = Project::valid()->active()->get()->sortBy(function ($project) {
-            [$major, $minor, $patch] = explode('.', $project->current_laravel_version);
+        $statusSorts = [Project::STATUS_INSECURE => -1, Project::STATUS_BEHIND => 0, Project::STATUS_CURRENT => 1];
 
-            // 1st Insecure | 2nd Behind | 3rd Current
-            if ($project->status === Project::STATUS_INSECURE) {
-                return 1000000 + ($major * 10000) + ($minor * 100) + (int) $patch;
-            }
-
-            if ($project->status === Project::STATUS_BEHIND) {
-                return 2000000 + ($major * 10000) + ($minor * 100) + (int) $patch;
-            }
-
-            return 3000000 + ($major * 100) + ($minor * 100) + (int) $patch;
-        });
+        $projects = Project::valid()->active()->get()->sortBy([
+            fn($a, $b) => $statusSorts[$a->status] <=> $statusSorts[$b->status],
+            fn($a, $b) => $a->current_laravel_version <=> $b->current_laravel_version,
+        ]);
 
         if (auth()->guest() && ! (config('app.show_private_repos'))) {
             $projects = $projects->reject->is_private;
